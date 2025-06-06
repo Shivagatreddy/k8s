9f5450b8821750558a82221990ec3f730a7f8c2b
bharath <shivaazuredevops9@gmail.com>
2025-06-06 16:25:59 +0530

    updated

---
+++ AWS/aws-terraform-demo/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.vpc_name
+    Project = "aws-learning"
+  }
+}
